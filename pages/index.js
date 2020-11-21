edc658913ca6a4bb03c4ad968d9bd8e8705e20d6
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-11-21 00:26:23 -0800

    creates src, moves api

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
+import styles from '../src/styles/Home.module.scss';
 
 export default function Home() {
   return (
