34c1db4b7f7f1c7038a6d4e46baa508610af73a1
jacob kleiman <jacobakleiman@gmail.com>
2020-11-23 11:17:05 -0800

    adds useUser hook

---
+++ hooks/useUser.js
@@ -0,0 +1,2 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
--- pages/api/auth/logout/index.js
+++ pages/api/auth/logout/index.js
@@ -2,41 +2,62 @@ import nc from 'next-connect';
 import bcrypt from 'bcrypt';
 import User from '../../../../src/models/User';
 import dbConnect from '../../../../src/db/mongoose';
-import session from 'express-session';
-import connectMongo from 'connect-mongo';
+// import session from 'express-session';
+// import connectMongo from 'connect-mongo';
+import { withIronSession, ironSession } from 'next-iron-session';
 
-const MongoStore = connectMongo(session);
+const session = ironSession({
+  password: 'iaintneverseentwoprettybestfriends',
+  cookieName: 'user',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production' ? true : false,
+  },
+});
+// const MongoStore = connectMongo(session);
 
 const handler = nc()
-  .use(async (req, res, next) => {
-    return session({
-      store: new MongoStore({ url: process.env.MONGODB_URI }),
-      secret: 'iaintneverseentwoprettybestfriends',
-      resave: false,
-      saveUninitialized: true,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24,
-      },
-    })(req, res, next);
-    // next();
-  })
+  // .use(async (req, res, next) => {
+  //   return session({
+  //     store: new MongoStore({ url: process.env.MONGODB_URI }),
+  //     secret: 'iaintneverseentwoprettybestfriends',
+  //     resave: false,
+  //     saveUninitialized: true,
+  //     cookie: {
+  //       maxAge: 1000 * 60 * 60 * 24,
+  //     },
+  //   })(req, res, next);
+  //   // next();
+  // })
   .use(async (req, res, next) => {
     await dbConnect();
     next();
   })
+  .use(session)
   .delete(async (req, res) => {
-    console.log(req.session);
-    if (!req.session.currentUser)
-      return res.status(401).json({
-        message: 'No user to log out!',
-      });
-    req.session.destroy((err) => {
-      if (err)
-        return res.status(500).json({
-          message: 'Something went wrong. Please try again.',
-        });
-      res.sendStatus(200);
-    });
+    // const storedUser = req.session.get('currentUser');
+    // console.log(storedUser);
+    // if (!storedUser)
+    //   return res.status(401).json({
+    //     message: 'No user to log out!',
+    //   });
+    // req.session.destroy((err) => {
+    //   if (err)
+    //     return res.status(500).json({
+    //       message: 'Something went wrong. Please try again.',
+    //     });
+    //   res.sendStatus(200);
+    // });
+    // console.log(req.session.get('currentUser'));
+    req.session.destroy();
+    res.json({ status: 200, message: 'logged out' });
   });
+
+// export default withIronSession(handler, {
+//   password: 'iaintneverseentwoprettybestfriends',
+//   cookieName: 'user',
+//   cookieOptions: {
+//     maxAge: 1000 * 60 * 60 * 24,
+//   },
+// });
 
 export default handler;
