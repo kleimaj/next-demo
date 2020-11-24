f7122f9af1c8ea903e4c1507e88b658f0955f5ba
jacob kleiman <jacobakleiman@gmail.com>
2020-11-23 21:11:03 -0800

    adds images, button active states, emotion components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,39 +1,12 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../src/styles/Home.module.scss';
+import Quiz from '../src/components/Quiz';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Next App</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-          <Link href='/notes'>
-            <a>Check out Todos</a>
-          </Link>
-        </p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
+      <Quiz />
     </div>
   );
 }
