4c73d9ecce2a70eda931834156372a30328c5276
jacob kleiman <jacobakleiman@gmail.com>
2020-11-23 16:36:22 -0800

    adds images

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
 import styled from '@emotion/styled';
+import Image from 'next/image';
 
 const Question = styled.div``;
 
 const quiz = [
   {
     question: 'What color is your grass?',
-    answers: [],
-    values: ['', '', '', ''],
+    type: 'image',
+    answers: ['Light Green', 'Medium Green', 'Dark Green', 'Blue Green'],
+    src: [
+      '/grass/lightgreen.png',
+      '/grass/mediumgreen.png',
+      '/grass/darkgreen.png',
+      '/grass/bluegreen.png',
+    ], //in public
+    values: ['lightgreen', 'mediumgreen', 'darkgreen', 'bluegreen'],
   },
   {
     question: 'Does your grass grow in the summer?',
+    type: 'radio',
     answers: ['Yes, I always have to mow it!', 'No, my mower gets a break'],
-    values: ['', ''],
+    values: ['warm', 'cool'],
   },
   {
     question: 'VERNATION',
@@ -33,5 +42,9 @@ const quiz = [
 export default function () {
   const [values, setValues] = useState({});
 
-  return <form></form>;
+  return (
+    <form>
+      <h2>Identify your grass</h2>
+    </form>
+  );
 }
