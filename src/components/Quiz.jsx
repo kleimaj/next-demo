fe3d572a538e147819f92224545d67f1f767bc87
jacob kleiman <jacobakleiman@gmail.com>
2020-11-23 17:38:54 -0800

    adds grass blade images

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -8,6 +8,7 @@ const quiz = [
   {
     question: 'What color is your grass?',
     type: 'image',
+    name: 'color',
     answers: ['Light Green', 'Medium Green', 'Dark Green', 'Blue Green'],
     src: [
       '/grass/lightgreen.png',
@@ -20,21 +21,25 @@ const quiz = [
   {
     question: 'Does your grass grow in the summer?',
     type: 'radio',
+    name: 'season',
     answers: ['Yes, I always have to mow it!', 'No, my mower gets a break'],
     values: ['warm', 'cool'],
   },
   {
     question: 'VERNATION',
+    name: 'vernation',
     answers: ['rolled', 'folded'],
     values: ['rolled', 'folded'],
   },
   {
     question: 'What shape is the tip of the blade of grass?',
+    name: 'tipShape',
     answers: [''],
     values: [''],
   },
   {
     question: 'How does your grass grow?',
+    name: 'growthHabit',
     answers: [''],
     values: [''],
   },
@@ -45,6 +50,26 @@ export default function () {
   return (
     <form>
       <h2>Identify your grass</h2>
+      <p>Answer 5 questions, watch the match</p>
+      {quiz.map((q, idx) => {
+        <Question>
+          <h3>
+            {idx + 1}. {q.question}
+          </h3>
+          {q.type === 'image'
+            ? q.src.map((src) => {
+                <button onClick={() => setValues()}>
+                  <Image alt='Grass' src={src} width={82} height={145} />;
+                </button>;
+              })
+            : q.values.map((val, i) => {
+                <>
+                  <label>{q.question[i]}</label>
+                  <input type='radio' name={q.name} value={val} />
+                </>;
+              })}
+        </Question>;
+      })}
     </form>
   );
 }
