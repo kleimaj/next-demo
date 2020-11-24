9d4dd0ac342fd49c26d6988a10e68c08d3dfad1a
uyen <ulam10@gmail.com>
2020-11-23 21:29:42 -0800

    signup complete with password confirmation & error handling

--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -1,23 +1,51 @@
 import styled from "@emotion/styled";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import next from "next";
+import { match } from "assert";
 
 const FormWrapper = styled.div`
-  display: flex;
   position: absolute;
-  top: 150px;
-  left: 120px;
+  top: 15%;
+  left: 25%;
+`;
+const Header = styled.h3`
+  font-size: 2rem;
+  text-align: center;
+  color: #323232;
+`;
+const Form = styled.form`
+  width: 50vw;
+  height: 50vh;
+  padding: 20px;
+  background-color: #ffa41b;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  display: flex;
   justify-content: center;
   align-items: center;
-  width: 200px;
-  padding: 20px;
-  background-color: lightgreen;
-  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-  border-radius: 15px;
+  flex-direction: column;
 `;
-
-const Header = styled.h3`
-  font-size: 1.5rem;
+const Input = styled.input`
+  border: none;
+  border-radius: 13px;
+  width: 100%;
+  padding: 8px 40px;
+  margin: 5px;
+  font-size: 1rem;
+  background-color: #fff;
+`;
+const Button = styled.button`
+  display: block;
+  border-radius: 5px;
+  border: none;
+  background-color: #000;
+  cursor: pointer;
+  margin-top: 15px;
+  padding: 5px 15px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.12);
+  color: #fff;
+  font-size: 1rem;
 `;
 
 const Signup = () => {
@@ -25,69 +53,101 @@ const Signup = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
-  // add regex to validate email and password here
+  // const matchPasswords = ({ password, password2 }) => {
+  //   console.log("hi");
+  //   if (password !== password2) {
+  //     setErrorMsg("Your passwords don't match. Please try again");
+  //   }
+  // };
 
   const handleSubmit = e => {
+    console.log("entering submit");
     e.preventDefault();
-    fetch("/api/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        password
-      })
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log("********", data);
 
-        // send them home or to the user profile?
-        // router.push("/");
+    // checking if passwords match
+    if (password !== password2) {
+      setErrorMsg("Your passwords don't match. Please try again");
+    } else {
+      setErrorMsg("");
+      fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        })
       })
-      .catch(err => console.log(err));
+        .then(res => res.json())
+        .then(data => {
+          // console.log("********", data);
+          // send them home or to the user profile?
+          router.push("/");
+        })
+        .catch(err => console.log(err));
+    }
   };
+
   return (
     <FormWrapper>
-      <form onSubmit={handleSubmit}>
-        <Header>Sign Up</Header>
+      <Header>Sign Up</Header>
+      {errorMsg ? <p style={{ color: "red" }}>{errorMsg}</p> : null}
+      <Form onSubmit={handleSubmit}>
         <label htmlFor='name'>
-          Name
-          <input
+          <Input
+            placeholder='Name'
             value={name}
             onChange={e => setName(e.target.value)}
             name='name'
             type='text'
+            autoComplete='name'
+            required
           />
         </label>
-        <br />
+
         <label htmlFor='email'>
-          Email
-          <input
+          <Input
+            placeholder='Email address'
             value={email}
             onChange={e => setEmail(e.target.value)}
             name='email'
             type='email'
+            autoComplete='email'
+            required
           />
         </label>
-        <br />
+
         <label htmlFor='password'>
-          Password
-          <input
+          <Input
+            placeholder='Create a password'
             value={password}
             onChange={e => setPassword(e.target.value)}
             name='password'
-            type='text'
+            type='password'
+            autoComplete='create-password'
+            required
           />
         </label>
-        <br />
 
-        <button type='submit'>Submit</button>
-        {/* {signupError && <p style={{ color: "red" }}>{signupError}</p>} */}
-      </form>
+        <label htmlFor='password2'>
+          <Input
+            placeholder='Confirm password'
+            value={password2}
+            onChange={e => setPassword2(e.target.value)}
+            name='password2'
+            type='password'
+            autoComplete='confirm-password'
+            required
+          />
+        </label>
+
+        <Button type='submit'>Submit</Button>
+      </Form>
     </FormWrapper>
   );
 };
