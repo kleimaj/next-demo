a955ec1f7fdb5291c8a01db4b10c40882fe17b8f
jacob kleiman <jacobakleiman@gmail.com>
2020-11-24 14:33:28 -0800

    removes ...notes-params

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -13,6 +13,9 @@ const Container = styled.div`
   margin-right: auto;
   margin-top: 1rem;
 `;
+const Logo = styled.h1`
+  font-family: Pacifico;
+`;
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -21,13 +24,15 @@ function MyApp({ Component, pageProps }) {
         <link rel='icon' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
-          href='https://fonts.googleapis.com/css2?family=PT+Serif&family=Poppins:ital,wght@0,400;0,700;1,300;1,400&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Londrina+Solid&family=PT+Serif&family=Pacifico&family=Poppins:ital,wght@0,400;0,700;1,300;1,400&display=swap'
           rel='stylesheet'
         />
       </Head>
       <Navbar>
         <Link href='/'>
-          <a></a>
+          <a>
+            <Logo>Otherside</Logo>
+          </a>
         </Link>
         <ul>
           <li>
--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -1,11 +1,18 @@
-import styled from "@emotion/styled";
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import styled from '@emotion/styled';
+import {
+  Card,
+  CardContainer,
+  CardContent,
+  CardHeader,
+} from '../../src/components/Card';
+import { PrimaryButton } from '../../src/components/Buttons';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const FormWrapper = styled.div`
-  position: absolute;
-  top: 15%;
-  left: 25%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
 const Header = styled.h3`
   font-size: 2rem;
@@ -13,17 +20,17 @@ const Header = styled.h3`
   color: #323232;
 `;
 const Form = styled.form`
-  width: 50vw;
-  height: 50vh;
-  padding: 20px;
-  /* background-color: #ffa41b; */
-  background-color: #73b899;
-  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.25);
-  border-radius: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  // width: 50vw;
+  // height: 50vh;
+  // padding: 20px;
+  // /* background-color: #ffa41b; */
+  // background-color: #73b899;
+  // box-shadow: 0 5px 16px rgba(0, 0, 0, 0.25);
+  // border-radius: 20px;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+  // flex-direction: column;
 `;
 const Input = styled.input`
   border: none;
@@ -32,7 +39,13 @@ const Input = styled.input`
   padding: 8px 40px;
   margin: 5px;
   font-size: 1rem;
-  background-color: #fff;
+  background: #f2f2f2;
+`;
+const Label = styled.label`
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
 `;
 const Button = styled.button`
   display: block;
@@ -49,94 +62,91 @@ const Button = styled.button`
 
 const Signup = () => {
   const router = useRouter();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const [errorMsg, setErrorMsg] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     // checking if passwords match
     if (password !== password2) {
       setErrorMsg("Your passwords don't match. Please try again");
     } else {
-      setErrorMsg("");
-      fetch("/api/auth/register", {
-        method: "POST",
+      setErrorMsg('');
+      fetch('/api/auth/register', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           name,
           email,
-          password
-        })
+          password,
+        }),
       })
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
           //* send them home or to the user profile?
-          router.push("/");
+          router.push('/');
         })
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
     }
   };
 
   return (
     <FormWrapper>
-      <Header>Sign Up</Header>
-      {errorMsg ? <p style={{ color: "red" }}>{errorMsg}</p> : null}
+      <Header>Create Account</Header>
+      {errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p> : null}
       <Form onSubmit={handleSubmit}>
-        <label htmlFor='name'>
-          <Input
-            placeholder='Name'
-            value={name}
-            onChange={e => setName(e.target.value)}
-            name='name'
-            type='text'
-            autoComplete='name'
-            required
-          />
-        </label>
+        <Card>
+          <CardHeader>Some personal info</CardHeader>
+          <CardContainer column>
+            <Label>Your name</Label>
+            <Input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              name='name'
+              type='text'
+              autoComplete='name'
+              required
+            />
 
-        <label htmlFor='email'>
-          <Input
-            placeholder='Email address'
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            name='email'
-            type='email'
-            autoComplete='email'
-            required
-          />
-        </label>
+            <Label>Your email</Label>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name='email'
+              type='email'
+              autoComplete='email'
+              required
+            />
 
-        <label htmlFor='password'>
-          <Input
-            placeholder='Create a password'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            name='password'
-            type='password'
-            autoComplete='create-password'
-            required
-          />
-        </label>
+            <Label>Password</Label>
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name='password'
+              type='password'
+              autoComplete='create-password'
+              required
+            />
 
-        <label htmlFor='password2'>
-          <Input
-            placeholder='Confirm password'
-            value={password2}
-            onChange={e => setPassword2(e.target.value)}
-            name='password2'
-            type='password'
-            autoComplete='confirm-password'
-            required
-          />
-        </label>
+            <Label>Verify Password</Label>
+            <Input
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
+              name='password2'
+              type='password'
+              autoComplete='confirm-password'
+              required
+            />
 
-        <Button type='submit'>Submit</Button>
+            <PrimaryButton type='submit'>Submit</PrimaryButton>
+          </CardContainer>
+        </Card>
       </Form>
     </FormWrapper>
   );
